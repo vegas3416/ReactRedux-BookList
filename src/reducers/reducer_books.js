ae4b02b3453afe9320316b9f4172311eaf1619d6
vegas3416 <vegas3416@gmail.com>
2017-05-28 18:36:45 -0500

    Test

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+   return [
+      { title: 'Javascript' },
+      { title: 'hello' },
+      { title: 'Cars' },
+      { title: 'Dogs' },
+      { title: 'Boats' }
+   ]
+}
