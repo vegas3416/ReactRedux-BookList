803438c5253ea90dc87036c94c2b0723d09e3e25
vegas3416 <vegas3416@gmail.com>
2017-05-28 21:30:08 -0500

    Finished up actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,10 @@
 //action creators
 
 export function selectBook (book) {
-   console.log('A book has been selected:', book.title);
+   //selectBook is an ActionCreator, needs to return an action,
+   //an object with a type property.
+   return {
+    type: 'BOOK_SELECTED',
+    payload: book
+   };
 }
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function() {
    return [
-      { title: 'Javascript' },
-      { title: 'hello' },
-      { title: 'Cars' },
-      { title: 'Dogs' },
-      { title: 'Boats' }
+      { title: 'Javascript' , pages: 100 },
+      { title: 'helloSSS', pages: 300 },
+      { title: 'Cars', pages: 1 },
+      { title: 'Dogs', pages: 100 },
+      { title: 'Boats', pages: 44 }
    ]
 }
